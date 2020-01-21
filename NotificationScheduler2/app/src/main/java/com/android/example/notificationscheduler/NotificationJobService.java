0d7671fc91806a70017132bb252252028faca093
Prajuk Nusbaum <deomax@yahoo.com>
2020-01-20 18:59:05 -0600

    working project

--- NotificationScheduler2/app/src/main/java/com/android/example/notificationscheduler/NotificationJobService.java
+++ NotificationScheduler2/app/src/main/java/com/android/example/notificationscheduler/NotificationJobService.java
@@ -4,6 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
 import android.app.job.JobService;
 import android.content.Intent;
 import android.graphics.Color;
@@ -11,7 +12,7 @@ import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
-public class NotificationJobServices extends JobService {
+public class NotificationJobService extends JobService {
     NotificationManager mNotifyManager;
 
     // Notification channel ID.
