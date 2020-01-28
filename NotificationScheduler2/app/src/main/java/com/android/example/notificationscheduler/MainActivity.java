47ab6c2664ef458e1b217515a013596ab7168cc8
Prajuk Nusbaum <deomax@yahoo.com>
2020-01-27 19:23:57 -0600

    Toast Challenge - Vertical or Horizontal changes

--- NotificationScheduler2/app/src/main/java/com/android/example/notificationscheduler/MainActivity.java
+++ NotificationScheduler2/app/src/main/java/com/android/example/notificationscheduler/MainActivity.java
@@ -8,17 +8,51 @@ import android.content.ComponentName;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private JobScheduler mScheduler;
     private static final int JOB_ID = 0;
+    //Switches for setting job options
+    private Switch mDeviceIdleSwitch;
+    private Switch mDeviceChargingSwitch;
+
+    //Override deadline seekbar
+    private SeekBar mSeekBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDeviceIdleSwitch = findViewById(R.id.idleSwitch);
+        mDeviceChargingSwitch = findViewById(R.id.chargingSwitch);
+
+        mSeekBar = findViewById(R.id.seekBar);
+        final TextView seekBarProgress = findViewById(R.id.seekBarProgress);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (i > 0){
+                    seekBarProgress.setText(i + " s");
+                }else {
+                    seekBarProgress.setText("Not Set");
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
 
@@ -26,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
         RadioGroup networkOptions = findViewById(R.id.networkOptions);
         int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
         int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        int seekBarInteger = mSeekBar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
 
         mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
 
@@ -44,15 +80,41 @@ public class MainActivity extends AppCompatActivity {
         ComponentName serviceName = new ComponentName(getPackageName(),
                 NotificationJobService.class.getName());
         JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
-                .setRequiredNetworkType(selectedNetworkOption);
+                .setRequiredNetworkType(selectedNetworkOption)
+                .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked())
+                .setRequiresCharging(mDeviceChargingSwitch.isChecked());
 
-        JobInfo myJobInfo = builder.build();
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
+// Original
+//      boolean constraintSet = selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE;
+/* 2nd modification
+
+        boolean constraintSet = (selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE)
+                || mDeviceChargingSwitch.isChecked() || mDeviceIdleSwitch.isChecked();
+*/
+        boolean constraintSet = selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE
+                || mDeviceChargingSwitch.isChecked() || mDeviceIdleSwitch.isChecked()
+                || seekBarSet;
+
+  /*      JobInfo myJobInfo = builder.build();
+
         mScheduler.schedule(myJobInfo);
 
         Toast.makeText(this, "Job Scheduled, job will run when " +
                 "the constraints are met.", Toast.LENGTH_SHORT).show();
-
-
+*/
+        if(constraintSet) {
+            //Schedule the job and notify the user
+            JobInfo myJobInfo = builder.build();
+            mScheduler.schedule(myJobInfo);
+            Toast.makeText(this, "Job Scheduled, job will run when " +
+                    "the constraints are met.", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Please set at least one constraint",
+                    Toast.LENGTH_SHORT).show();
+        }
 
     }
 
